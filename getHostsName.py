6ace995e10592b4537818e98d69b14f051ade61a
Douglas Schilling Landgraf <dougsland@redhat.com>
2011-09-11 01:17:46 -0300

    Adding 3 more scripts to list Name of Clusters, DataCenters, Hosts

---
+++ getHostsName.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+#
+# Copyright (C) 2011
+#
+# Douglas Schilling Landgraf <dougsland@redhat.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+import urllib2
+import sys
+import base64
+from xml.etree import ElementTree
+
+# Example
+ADDR     = "192.168.123.176"
+API_PORT = "8443"
+USER     = "rhevm@ad.rhev3.com"  # Must provide @domain
+PASSWD   = "T0pSecreT!"
+
+# Setting URL
+URL      = "https://" + ADDR + ":" + API_PORT + "/api/hosts"
+
+request = urllib2.Request(URL)
+print "Connecting to: " + URL
+
+base64string = base64.encodestring('%s:%s' % (USER, PASSWD)).strip()
+request.add_header("Authorization", "Basic %s" % base64string)
+
+try:
+        xmldata = urllib2.urlopen(request).read()
+except urllib2.URLError, e:
+        print "Error: cannot connect to REST API: %s" % (e)
+        print "Try to login using the same user/pass by the Admin Portal and check the error!"
+        sys.exit(2)
+
+tree = ElementTree.XML(xmldata)
+lst = tree.findall("host/name")
+
+#print len(lst)
+
+for item in lst:
+	print item.text
