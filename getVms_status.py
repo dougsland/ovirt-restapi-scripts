d042f3c3dbab9d025908bf86dce2095a364964a9
Amador Pahim <amador@pahim.org>
2011-11-21 16:41:29 -0200

    iGetting VMs status.

---
+++ getVms_status.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+#
+# Copyright (C) 2011
+#
+# Douglas Schilling Landgraf <dougsland@redhat.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+import urllib2
+import base64
+import sys
+from xml.etree import ElementTree
+
+# Example
+ADDR     = "192.168.123.176"
+API_PORT = "8443"
+USER     = "rhevm@ad.rhev3.com"
+PASSWD   = "T0pSecreT!"
+
+# Setting URL
+URL      = "https://" + ADDR + ":" + API_PORT + "/api/vms"
+
+request = urllib2.Request(URL)
+print "Connecting to: %s\n" % (URL)
+
+base64string = base64.encodestring('%s:%s' % (USER, PASSWD)).strip()
+request.add_header("Authorization", "Basic %s" % base64string)
+
+try:
+        xmldata = urllib2.urlopen(request).read()
+except urllib2.URLError, e:
+        print "Error: cannot connect to REST API: %s" % (e)
+        print "Try to login using the same user/pass by the Admin Portal and check the error!"
+        sys.exit(2)
+
+tree = ElementTree.XML(xmldata)
+list = tree.findall("vm")
+
+#print len(list)
+
+for item in list:
+        for subitem in item.findall("status"):
+        	print "%s - %s"      % (item.find("name").text,subitem.find("state").text)
