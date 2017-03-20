ff492dac480a4b51c683d56430d57b24afbce175
Douglas Schilling Landgraf <dougsland@gmail.com>
2017-03-20 16:17:28 -0400

    getClusterName: update script

--- getClusterName.py
+++ getClusterName.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 #
-# Copyright (C) 2011
+# Copyright (C) 2011-2017
 #
 # Douglas Schilling Landgraf <dougsland@redhat.com>
 #
@@ -16,34 +16,39 @@
 import urllib2
 import base64
 import sys
+import ssl
 from xml.etree import ElementTree
 
 # Example
-ADDR     = "192.168.123.176"
-API_PORT = "8443"
-USER     = "rhevm@ad.rhev3.com"  # Must provide @DOMAIN
-PASSWD   = "T0pSecreT!"
+ADDR = "192.168.122.116"
+API_PORT = "443"
+USER = "admin@internal"  # Must provide @DOMAIN
+PASSWD = "redhat"
 
 # Setting URL
-URL      = "https://" + ADDR + ":" + API_PORT + "/api/clusters"
+URL = "https://" + ADDR + ":" + API_PORT + "/api/clusters"
 
 request = urllib2.Request(URL)
-print "Connecting to: " + URL
+print("Connecting to: %s" % URL)
 
 base64string = base64.encodestring('%s:%s' % (USER, PASSWD)).strip()
 request.add_header("Authorization", "Basic %s" % base64string)
 
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
 try:
-	xmldata = urllib2.urlopen(request).read()
+    xmldata = urllib2.urlopen(request, context=ctx).read()
 except urllib2.URLError, e:
-	print "Error: cannot connect to REST API: %s" % (e)
-	print "Try to login using the same user/pass by the Admin Portal and check the error!"
-	sys.exit(2)
+    print("Error: cannot connect to REST API: %s" % (e))
+    print("Try to login using the same user/pass by "
+          "the Admin Portal and check the error!")
+    sys.exit(2)
 
 tree = ElementTree.XML(xmldata)
 list = tree.findall("cluster/name")
 
-#print len(lst)
-
+print("\nClusters found:")
 for item in list:
-	print item.text
+    print("* %s" % item.text)
